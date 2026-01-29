cfa5d040a3865f83a8ea7ec27d58ff25017a90f9
Doohaey Re <doohaey@gmail.com>
2026-01-30 02:13:46 +0800

    feat: add onAdvancementDone

--- src/main/kotlin/org/imyvm/cac/entrypoints/listeners/PlayerListener.kt
+++ src/main/kotlin/org/imyvm/cac/entrypoints/listeners/PlayerListener.kt
@@ -1,9 +1,14 @@
 package org.imyvm.cac.entrypoints.listeners
 
+import org.bukkit.GameMode
+import org.bukkit.advancement.Advancement
+import org.bukkit.entity.Player
 import org.bukkit.event.Listener
 import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerAdvancementDoneEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.imyvm.cac.CreativeAdvancementCompletion
+import org.imyvm.cac.domain.event.EventStatus
 import org.imyvm.cac.util.i18n.Translator
 
 class PlayerListener(
@@ -13,5 +18,27 @@ class PlayerListener(
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val player = event.player
         Translator.send(player, "player.join", player.name)
+    }
+
+    @EventHandler
+    fun onAdvancementDone(event: PlayerAdvancementDoneEvent) {
+        val player = event.player
+        val advancement = event.advancement
+
+        if (!shouldAllowAdvancement(player)) {
+            revokeAdvancement(player, advancement)
+        }
+    }
+
+    private fun shouldAllowAdvancement(player: Player): Boolean {
+        if (!EventStatus.isActive()) return false
+
+        if (player.gameMode != GameMode.CREATIVE) return false
+
+        return true
+    }
+
+    private fun revokeAdvancement(player: Player, advancement: Advancement) {
+        TODO()
     }
 }
