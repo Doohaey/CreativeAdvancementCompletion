fbe58eb0213465df1fed46677849066388f4164a
Doohaey Re <doohaey@gmail.com>
2026-01-30 15:16:38 +0800

    feat: add remove item if necessary

--- src/main/kotlin/org/imyvm/cac/application/AdvancementProgressHandler.kt
+++ src/main/kotlin/org/imyvm/cac/application/AdvancementProgressHandler.kt
@@ -1,6 +1,7 @@
 package org.imyvm.cac.application
 
 import org.bukkit.GameMode
+import org.bukkit.Material
 import org.bukkit.advancement.Advancement
 import org.bukkit.entity.Player
 import org.imyvm.cac.domain.event.EventStatus
@@ -10,6 +11,7 @@ class AdvancementProgressHandler {
     fun validateAndProcess(player: Player, advancement: Advancement) {
         if (!shouldAllow(player)) {
             resetProgress(player, advancement)
+            removeItemIfNecessary(player, advancement)
         }
     }
 
@@ -20,5 +22,33 @@ class AdvancementProgressHandler {
     private fun resetProgress(player: Player, advancement: Advancement) {
         val progress = player.getAdvancementProgress(advancement)
         progress.awardedCriteria.forEach { progress.revokeCriteria(it) }
+    }
+
+    private fun removeItemIfNecessary(player: Player, advancement: Advancement) {
+        val key = advancement.key.toString()
+        val materialsToRemove = getItemsForAdvancement(key) ?: return
+
+        val inventory = player.inventory
+        val contents = inventory.contents
+
+        for (i in contents.indices) {
+            val item = contents[i] ?: continue
+
+            if (materialsToRemove.contains(item.type)) {
+                inventory.setItem(i, null)
+            }
+        }
+    }
+
+    private fun getItemsForAdvancement(key: String): List<Material>? {
+        return when (key) {
+            "minecraft:story/root" -> listOf(Material.CRAFTING_TABLE)
+            "minecraft:story/mine_stone" -> listOf(Material.COBBLESTONE, Material.BLACKSTONE, Material.COBBLED_DEEPSLATE)
+            "minecraft:story/upgrade_tool" -> listOf(Material.STONE_PICKAXE)
+            "minecraft:story/smelt_iron" -> listOf(Material.IRON_INGOT)
+            "minecraft:story/iron_tools" -> listOf(Material.IRON_PICKAXE)
+            "minecraft:nether/get_wither_skull" -> listOf(Material.WITHER_SKELETON_SKULL)
+            else -> null
+        }
     }
 }
